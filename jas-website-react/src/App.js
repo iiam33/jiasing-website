0bc80833c486363c592685885aadc6c7fe11a8f9
iiam33 <alvinyeoh0318@gmail.com>
2022-11-07 12:20:56 +0800

    create react app

---
+++ jas-website-react/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ jas-website-react/src/components/LandingSection.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./LandingSection.css";
+
+function LandingSection() {
+  return <></>;
+}
