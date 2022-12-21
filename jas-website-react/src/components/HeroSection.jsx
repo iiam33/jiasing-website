d653bce0588f6966e1c7d26f308077a3ed830096
iiam33 <alvinyeoh0318@gmail.com>
2022-12-21 10:10:44 +0800

    done blinking background

---
+++ jas-website-react/src/components/HeroSection.jsx
@@ -0,0 +1,127 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { useSpring, animated } from "react-spring";
+
+import AnimatedLogo from "./AnimatedLogo";
+import AnimatedStar from "./AnimatedStar";
+
+import name from "../assets/images/JiaSing_Namesvg.svg";
+
+import "./css/HeroSection.css";
+
+function FadeUpName({ scrollDown }) {
+  const nameStyles = useSpring({
+    from: { opacity: 0, y: -30 },
+    to: {
+      opacity: scrollDown ? 1 : 0,
+      y: scrollDown ? -10 : 50,
+    },
+    delay: scrollDown ? 100 : 0,
+  });
+
+  return (
+    <animated.div
+      style={{
+        marginLeft: "auto",
+        marginRight: "auto",
+        maxWidth: "11vw",
+        ...nameStyles,
+      }}
+    >
+      <img className="name" src={name} alt="Name" />
+    </animated.div>
+  );
+}
+
+function FadeUpJobTitle({ scrollDown }) {
+  const titleStyles = useSpring({
+    from: { opacity: 0, y: 0 },
+    to: {
+      opacity: scrollDown ? 1 : 0,
+      y: scrollDown ? 0 : 50,
+    },
+    delay: scrollDown ? 250 : 0,
+  });
+
+  return (
+    <animated.div
+      style={{
+        marginLeft: "auto",
+        marginRight: "auto",
+        maxWidth: "60vw",
+        ...titleStyles,
+      }}
+    >
+      {/* <img className="title" src={title} alt="Title" /> */}
+      <div className="hero-job-title">
+        <div className="job-title">
+          <p>Concept Artist / Illustration</p>
+        </div>
+      </div>
+    </animated.div>
+  );
+}
+
+function HeroSection({ isActive }) {
+  const [scrollDown, setScrollDown] = useState(false);
+
+  const translateStyles = useSpring({
+    from: { y: 120, maxWidth: "20vw" },
+    to: { y: scrollDown ? -10 : 120, maxWidth: scrollDown ? "10vw" : "20vw" },
+  });
+
+  // const shrinkStyles = useSpring({
+  //   from: { maxHeight: "100vh" },
+  //   to: { maxHeight: scrollDown ? "90vh" : "100vh" },
+  // });
+
+  useEffect(() => {
+    const handleScroll = (event) => {
+      if (event.deltaY > 0) {
+        setScrollDown(true);
+      } else {
+        setScrollDown(false);
+      }
+    };
+
+    window.addEventListener("wheel", handleScroll);
+
+    return () => {
+      window.removeEventListener("wheel", handleScroll);
+    };
+  }, [scrollDown]);
+
+  return (
+    <>
+      {/* <animated.div
+        style={{
+          ...shrinkStyles,
+        }}
+        className="hero-wrapper"
+      > */}
+      <div className="hero-wrapper">
+        <div className="star">
+          <AnimatedStar isActive={isActive} />
+        </div>
+        <div className="hero-container">
+          <animated.div
+            style={{
+              marginLeft: "auto",
+              marginRight: "auto",
+              ...translateStyles,
+            }}
+          >
+            <div className="logo">
+              <AnimatedLogo />
+            </div>
+          </animated.div>
+          <FadeUpName scrollDown={scrollDown} />
+          <FadeUpJobTitle scrollDown={scrollDown} />
+        </div>
+      </div>
+      {/* </animated.div> */}
+    </>
+  );
+}
+
+export default HeroSection;
